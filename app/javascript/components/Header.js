23a263a21d046a0bd2d301160c439a8faa0b1012
gonuma <81454703+gonuma@users.noreply.github.com>
2021-09-20 22:27:51 +0900

    Add redux tomorrow

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -1,13 +1,23 @@
 import React from "react";
+import axios from "axios";
 
 export default function Header() {
+  const uploadSong = () => {
+    let band = document.getElementById("bandInput").value;
+    let title = document.getElementById("titleInput").value;
+    document.getElementById("bandInput").value = null;
+    document.getElementById("titleInput").value = null;
+    console.log(band, title);
+    axios.post("/api/v1/songs", { band: band, title: title });
+  };
+
   return (
     <div className="header">
       <h1>MusiSpace 2.0</h1>
       <div className="inputArea">
-        <button onClick={() => console.log("End me now")}>End Me</button>
-        <input placeholder="Band Name"></input>
-        <input placeholder="Song Name"></input>
+        <button onClick={() => uploadSong()}>Post Song</button>
+        <input placeholder="Band Name" id="bandInput"></input>
+        <input placeholder="Song Name" id="titleInput"></input>
       </div>
     </div>
   );
