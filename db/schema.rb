c994a1b7455eccff70a0436525e98172f4faa105
gonuma <81454703+gonuma@users.noreply.github.com>
2021-09-24 16:18:16 +0900

    Not fully implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_233905) do
+ActiveRecord::Schema.define(version: 2021_09_24_071034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2021_09_23_233905) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["song_id"], name: "index_comments_on_song_id"
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "songs", force: :cascade do |t|
